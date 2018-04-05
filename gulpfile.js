0aa5b02c121206357bf74edfc4a6b397aaa14b92
kamilkalicki <kmlkalicki@gmail.com>
2018-04-05 22:05:53 +0200

    project structure update, gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,36 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let cleanCSS = require('gulp-clean-css');
 let autoprefixer = require('gulp-autoprefixer');
-
 let babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
 
-gulp.task('sass', function () {
-  return gulp.src('./src/css/*.scss')
+gulp.task('sass', () => {
+  gulp.src('./src/css/all.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({compatibility: 'ie10'}))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('./src/css'));
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/css/*.scss', ['sass']);
+gulp.task('sass:watch', () => {
+  gulp.watch('./src/css/all.scss', ['sass']);
 });
 
-gulp.task('default', () =>
-  gulp.src('src/app.js')
+gulp.task('es6', () =>
+  gulp.src('./src/js/all.js')
     .pipe(babel({
       presets: ['env']
     }))
-    .pipe(gulp.dest(''))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'))
 );
+
+gulp.task('es6:watch', () => {
+  gulp.watch('./src/js/all.js', ['es6']);
+});
+
+gulp.task('build', ['sass', 'es6']);
+gulp.task('default', ['sass:watch', 'es6:watch']);
